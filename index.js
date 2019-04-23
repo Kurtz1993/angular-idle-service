f872ded45d55ac0c2e451ef2c040e0fb4689d3dc
Luis Rogelio Hernández López <luis.hlopez@outlook.com>
2019-04-23 17:54:58 -0500

    Adjust index file

--- index.js
+++ index.js
@@ -1,23 +1,26 @@
 angular.module('testApp', ['$idle'])
     .config(function ($keepaliveProvider, $idleProvider) {
-        $idleProvider.setTimeoutTime(3);
-        $idleProvider.setIdleTime(5);
-        $idleProvider.keepalive(true);
+        $idleProvider.setTimeoutTime(5);
+        $idleProvider.setIdleTime(10);
+        $idleProvider.keepalive(false);
         $keepaliveProvider.setInterval(5);
         $keepaliveProvider.http('http://localhost:8080');
     })
     .run(function ($keepalive, $rootScope, $idle) {
         //$keepalive.start();
+        console.log($idle)
         $idle.watch();
         $rootScope.$on('$keepalive', function () {
             console.log('Keeping alive...');
         });
         $rootScope.$on('$keepaliveResponse', function (data, status) {
-            console.log("Status: " + status);
+            console.log("Status: ");
+            console.log(status);
             console.log(data);
         });
         $rootScope.$on('$userIdle', function () {
             console.log('You are idle...');
+            console.log(`You have been idle for ${$idle.getIdleState().userIdleTime} seconds`);
         });
         $rootScope.$on('$userTimeout', function () {
             console.log('You timed out');
