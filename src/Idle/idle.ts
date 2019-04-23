2cfddde402efd355c6f62e8368f34d2abe9a00a7
Luis Rogelio Hernández López <luis.hlopez@outlook.com>
2019-04-23 17:55:13 -0500

    Add method to retrieve idle state

--- src/Idle/idle.ts
+++ src/Idle/idle.ts
@@ -9,28 +9,44 @@ module IdlePackage.Modules {
     private keepaliveService: IKeepaliveService;
     private storage: IIdleStorage;
     private id: number;
+    private idleCounter: ng.IPromise<any>;
 
     constructor() {
       this.options = {
         idle: 20 * 60, // Default idle time is 20 minutes.
-        timeout: 30,   // Default timeout time is 30 seconds.
-        autoResume: 'idle',
-        interrupt: 'mousemove keydown DOMMouseScroll mousewheel mousedown touchstart touchmove scroll',
+        timeout: 30, // Default timeout time is 30 seconds.
+        autoResume: "idle",
+        interrupt:
+          "mousemove keydown DOMMouseScroll mousewheel mousedown touchstart touchmove scroll",
         windowInterrupt: null,
-        keepalive: false  // By default the module won't send keepalive pings.
+        keepalive: false, // By default the module won't send keepalive pings.
       };
       this.state = {
         idle: null,
         timeout: null,
+        userIdleTime: 0,
         idling: false,
         running: false,
-        countdown: null
+        countdown: null,
       };
-      this.$get.$inject = ['$interval', '$rootScope', '$document', '$window', '$keepalive', '$idleLocalStorage'];
+      this.$get.$inject = [
+        "$interval",
+        "$rootScope",
+        "$document",
+        "$window",
+        "$keepalive",
+        "$idleLocalStorage",
+      ];
     }
 
-    $get($interval: ng.IIntervalService, $rootScope: ng.IRootScopeService, $document: ng.IDocumentService,
-      $window: ng.IWindowService, $keepalive: IKeepaliveService, $idleLocalStorage: IIdleStorage): IIdleService {
+    $get(
+      $interval: ng.IIntervalService,
+      $rootScope: ng.IRootScopeService,
+      $document: ng.IDocumentService,
+      $window: ng.IWindowService,
+      $keepalive: IKeepaliveService,
+      $idleLocalStorage: IIdleStorage
+    ): IIdleService {
       this.interval = $interval;
       this.rootScope = $rootScope;
       this.document = $document;
@@ -40,24 +56,29 @@ module IdlePackage.Modules {
       this.id = new Date().getTime();
       let lastmove = new LastMove();
 
-      this.document.find('html').on(this.options.interrupt, (event: JQueryEventObject) => {
-        if (event.type === 'mousemove' && event.originalEvent && event.originalEvent['movementX'] === 0 && event.originalEvent['movementY'] === 0) {
+      this.document.find("html").on(this.options.interrupt, (event: JQueryEventObject) => {
+        if (
+          event.type === "mousemove" &&
+          event.originalEvent &&
+          event.originalEvent["movementX"] === 0 &&
+          event.originalEvent["movementY"] === 0
+        ) {
           return;
         }
 
-        if (event.type !== 'mousemove' || lastmove.hasMoved(event)) {
+        if (event.type !== "mousemove" || lastmove.hasMoved(event)) {
           this.interruptTimers();
         }
       });
 
       if (this.options.windowInterrupt) {
         let fn = () => this.interruptTimers();
-        this.options.windowInterrupt.forEach((evt) => {
+        this.options.windowInterrupt.forEach(evt => {
           this.window.addEventListener(evt, fn, false);
         });
       }
 
-      this.window.addEventListener('storage', this.wrap.bind(this), false);
+      this.window.addEventListener("storage", this.wrap.bind(this), false);
 
       return {
         setTimeoutTime: this.setTimeoutTime.bind(this),
@@ -69,7 +90,8 @@ module IdlePackage.Modules {
         isIdling: () => this.state.idling,
         watch: this.watch.bind(this),
         unwatch: this.unwatch.bind(this),
-        interrupt: this.interruptTimers.bind(this)
+        interrupt: this.interruptTimers.bind(this),
+        getIdleState: () => this.state,
       };
     }
 
@@ -77,7 +99,7 @@ module IdlePackage.Modules {
       if (angular.isNumber(seconds) && seconds >= 0) {
         this.options.timeout = seconds;
       } else {
-        throw new Error('Timeout time must be a positive integer in seconds or 0 to disable it.');
+        throw new Error("Timeout time must be a positive integer in seconds or 0 to disable it.");
       }
     }
 
@@ -93,12 +115,12 @@ module IdlePackage.Modules {
       if (angular.isNumber(seconds) && seconds >= 0) {
         this.options.idle = seconds;
       } else {
-        throw new Error('Idle time must be a positive integer in seconds or 0 to disable it.');
+        throw new Error("Idle time must be a positive integer in seconds or 0 to disable it.");
       }
     }
 
     autoResume(autoResume: boolean): void {
-      this.options.autoResume = autoResume ? 'idle' : 'off';
+      this.options.autoResume = autoResume ? "idle" : "off";
     }
 
     keepalive(keepalive: boolean): void {
@@ -143,7 +165,7 @@ module IdlePackage.Modules {
         return;
       }
 
-      this.rootScope.$broadcast('$userIdleWarning', this.state.countdown);
+      this.rootScope.$broadcast("$userIdleWarning", this.state.countdown);
       this.state.countdown--;
     }
 
@@ -152,14 +174,14 @@ module IdlePackage.Modules {
      */
     private timeout(): void {
       this.stopKeepalive();
-      this.interval.cancel(this.state.idle);
-      this.interval.cancel(this.state.timeout);
+      this.stopTimers();
 
       this.state.idling = true;
       this.state.running = false;
       this.state.countdown = 0;
+      this.state.userIdleTime = 0;
 
-      this.rootScope.$broadcast('$userTimeout');
+      this.rootScope.$broadcast("$userTimeout");
     }
 
     /**
@@ -167,14 +189,19 @@ module IdlePackage.Modules {
      */
     private toggleState(): void {
       this.state.idling = !this.state.idling;
-      let name = this.state.idling ? '$userIdle' : '$userBack';
+      let name = this.state.idling ? "$userIdle" : "$userBack";
       if (this.state.idling) {
         this.rootScope.$broadcast(name);
         this.stopKeepalive();
         if (this.options.timeout) {
           this.state.countdown = this.options.timeout;
           this.countdown();
-          this.state.timeout = this.interval(this.countdown.bind(this), 1000, this.options.timeout, false);
+          this.state.timeout = this.interval(
+            this.countdown.bind(this),
+            1000,
+            this.options.timeout,
+            false
+          );
         }
       } else {
         this.startKeepalive();
@@ -203,7 +230,7 @@ module IdlePackage.Modules {
     }
 
     private wrap(event: any): void {
-      if (event.key === 'idle.expiry' && event.newValue && event.newValue !== event.oldValue) {
+      if (event.key === "idle.expiry" && event.newValue && event.newValue !== event.oldValue) {
         let val = angular.fromJson(event.newValue);
         if (val.id === this.id) {
           return;
@@ -217,7 +244,7 @@ module IdlePackage.Modules {
      * @returns The expiration date.
      */
     private getExpiry(): Date {
-      let obj = this.storage.get('expiry');
+      let obj = this.storage.get("expiry");
       return obj && obj.time ? new Date(obj.time) : null;
     }
 
@@ -227,9 +254,9 @@ module IdlePackage.Modules {
      */
     private setExpiry(date: Date): void {
       if (!date) {
-        this.storage.remove('expiry');
+        this.storage.remove("expiry");
       } else {
-        this.storage.set('expiry', { id: this.id, time: date });
+        this.storage.set("expiry", { id: this.id, time: date });
       }
     }
 
@@ -243,7 +270,11 @@ module IdlePackage.Modules {
         return;
       }
 
-      if (anotherTab || this.options.autoResume === 'idle' || (this.options.autoResume === 'notIdle' && !this.state.idling)) {
+      if (
+        anotherTab ||
+        this.options.autoResume === "idle" ||
+        (this.options.autoResume === "notIdle" && !this.state.idling)
+      ) {
         this.watch(anotherTab);
       }
     }
@@ -254,12 +285,12 @@ module IdlePackage.Modules {
     }
 
     private watch(noExpiryUpdate?: boolean): void {
-      this.interval.cancel(this.state.idle);
-      this.interval.cancel(this.state.timeout);
+      this.stopTimers();
+      this.state.userIdleTime = 0;
 
       let timeout = !this.options.timeout ? 0 : this.options.timeout;
       if (noExpiryUpdate) {
-        this.setExpiry(new Date(new Date().getTime() + ((this.options.idle + timeout) * 1000)));
+        this.setExpiry(new Date(new Date().getTime() + (this.options.idle + timeout) * 1000));
       }
 
       if (this.state.idling) {
@@ -269,18 +300,32 @@ module IdlePackage.Modules {
       }
 
       this.state.running = true;
-      this.state.idle = this.interval(this.toggleState.bind(this), this.options.idle * 1000, 0, false);
+      this.idleCounter = this.interval(() => this.state.userIdleTime++, 1000);
+      this.state.idle = this.interval(
+        this.toggleState.bind(this),
+        this.options.idle * 1000,
+        0,
+        false
+      );
     }
 
     private unwatch(): void {
-      this.interval.cancel(this.state.idle);
-      this.interval.cancel(this.state.timeout);
+      this.stopTimers();
 
       this.state.idling = false;
       this.state.running = false;
       this.setExpiry(null);
 
       this.stopKeepalive();
+    }
+
+    /**
+     * Stops all the intervals running.
+     */
+    private stopTimers(): void {
+      this.interval.cancel(this.state.idle);
+      this.interval.cancel(this.state.timeout);
+      this.interval.cancel(this.idleCounter);
     }
   } // End of class
 
@@ -298,10 +343,10 @@ module IdlePackage.Modules {
      * @event The mouse event containing the new coordinates.
      * @returns The swapped coordinates.
      */
-    swap(event: JQueryEventObject): { clientX: number, clientY: number } {
+    swap(event: JQueryEventObject): { clientX: number; clientY: number } {
       let last = {
         clientX: this.clientX,
-        clientY: this.clientY
+        clientY: this.clientY,
       };
 
       this.clientX = event.clientX;
@@ -317,10 +362,10 @@ module IdlePackage.Modules {
      */
     hasMoved(event: JQueryEventObject): boolean {
       let last = this.swap(event);
-      if (this.clientX === null || event['movementX'] || event['movementY']) {
+      if (this.clientX === null || event["movementX"] || event["movementY"]) {
         return true;
       } else if (last.clientX != event.clientX || last.clientY != event.clientY) {
-        return true
+        return true;
       } else {
         return false;
       }
